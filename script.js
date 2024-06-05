2e6614c87ea3fdd049b89334e2c320d0fc010bb9
Freakachu69 <151987985+Freakachu69@users.noreply.github.com>
2024-06-04 21:58:07 -0400

    Create function to play entire game Functions for human and computer choice needed to be moved inside playRound to prevent the first pair of choices from automatically becoming the choices for all five rounds. playRound now recalls itself upon inputting an invalid choice to prevent advancing the game on an error. Slight changes made to console messages for round results to avoid redundancy with console messages for the game result.

--- script.js
+++ script.js
@@ -1,49 +1,66 @@
-let humanScore = 0
-let computerScore = 0
+function playGame() {
 
-function getComputerChoice() {
-    Math.random()
+    let humanScore = 0
+    let computerScore = 0
 
-    if (Math.random() <= .333) {
-        return "Rock";
-    } else if (Math.random() <= .666) {
-        return "Paper";
+    function getComputerChoice() {
+        Math.random()
+
+        if (Math.random() <= .333) {
+            return "Rock";
+        } else if (Math.random() <= .666) {
+            return "Paper";
+        } else {
+            return "Scissors";
+        }
+    }
+
+    function getHumanChoice() {
+        input = prompt("Rock, paper, or scissors?")
+
+        input = input.charAt(0).toUpperCase() + input.slice(1).toLowerCase()
+        
+        if (input == "Rock" || input == "Paper" || input == "Scissors") {
+            return input;
+        }
+    }
+
+    let humanChoice
+    let computerChoice
+
+    function playRound(humanChoice,computerChoice) {
+        humanChoice = getHumanChoice()
+        computerChoice = getComputerChoice()
+        if (input != "Rock" && input != "Paper" && input != "Scissors") {
+            console.log("Error: Please choose rock, paper, or scissors.")
+            playRound(humanChoice,computerChoice)
+        } else if ((humanChoice === "Rock" && computerChoice === "Paper") || (humanChoice === "Paper" && computerChoice === "Scissors") || (humanChoice === "Scissors" && computerChoice === "Rock")) {
+            ++computerScore;
+            console.log("Ouch! " + computerChoice + " beats " + humanChoice + ".");
+            console.log ("You: " + humanScore + "\nComputer: " + computerScore);
+        } else if (humanChoice === computerChoice) {
+            console.log("Draw.");
+            console.log ("You: " + humanScore + "\nComputer: " + computerScore);
+        } else {
+            ++humanScore;
+            console.log("Score! " + humanChoice + " beats " + computerChoice + "!");
+            console.log ("You: " + humanScore + "\nComputer: " + computerScore);
+        }
+    }
+
+    playRound(humanChoice,computerChoice);
+    playRound(humanChoice,computerChoice);
+    playRound(humanChoice,computerChoice);
+    playRound(humanChoice,computerChoice);
+    playRound(humanChoice,computerChoice);
+
+    if (humanScore > computerScore) {
+        console.log("You Win!")
+    } else if (computerScore > humanScore) {
+        console.log("Computer Wins.")
     } else {
-        return "Scissors";
+        console.log("No one wins.")
     }
 }
 
-function getHumanChoice() {
-    input = prompt("Rock, paper, or scissors?")
-
-    input = input.charAt(0).toUpperCase() + input.slice(1).toLowerCase()
-    
-    if (input == "Rock" || input == "Paper" || input == "Scissors") {
-        return input;
-    }
-}
-
-let humanChoice = getHumanChoice()
-let computerChoice = getComputerChoice()
-
-function playRound(humanChoice, computerChoice) {
-    if ((humanChoice === "rock" && computerChoice === "paper") || (humanChoice === "paper" && computerChoice === "scissors") || (humanChoice === "scissors" && computerChoice === "rock")) {
-        ++computerScore;
-        console.log("You Lose. " + computerChoice + " beats " + humanChoice + ".");
-    } else if (humanChoice === computerChoice) {
-        console.log("Draw. No one wins.");
-    } else if (input != "Rock" && input != "Paper" && input != "Scissors") {
-        console.log("Error: Please choose rock, paper, or scissors.")
-    } else {
-        ++humanScore;
-        console.log("You Win! " + humanChoice + " beats " + computerChoice + "!");
-    }
-    
-    console.log ("You: " + humanScore + "\nComputer: " + computerScore)
-}
-
-    playRound(humanChoice, computerChoice)
-
-
-    //playGame()
-    //could be a loop? while humanScore != 5 || computerScore != 5
+playGame()
