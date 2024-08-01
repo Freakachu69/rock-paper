ecf7b488c3c8d325c0d6ee2ce4ce1182fed59edf
Freakachu69 <151987985+Freakachu69@users.noreply.github.com>
2024-08-01 00:13:17 -0400

    Replace console messages with DOM methods

--- script.js
+++ script.js
@@ -13,25 +13,40 @@
         }
     }
 
-    //let humanChoice
     let computerChoice
 
     function playRound(humanChoice,computerChoice) {
         computerChoice = getComputerChoice()
         if ((humanChoice === "Rock" && computerChoice === "Paper") || (humanChoice === "Paper" && computerChoice === "Scissors") || (humanChoice === "Scissors" && computerChoice === "Rock")) {
             ++computerScore;
-            console.log("Ouch! " + computerChoice + " beats " + humanChoice + ".");
-            console.log ("You: " + humanScore + "\nComputer: " + computerScore);
+            result.textContent = " Ouch! " + computerChoice + " beats " + humanChoice + ".";
+            score.textContent = "You: " + humanScore + "\nComputer: " + computerScore;
         } else if (humanChoice === computerChoice) {
-            console.log("Draw.");
-            console.log ("You: " + humanScore + "\nComputer: " + computerScore);
+            result.textContent = " Draw.";
+            score.textContent = "You: " + humanScore + "\nComputer: " + computerScore;
         } else {
             ++humanScore;
-            console.log("Score! " + humanChoice + " beats " + computerChoice + "!");
-            console.log ("You: " + humanScore + "\nComputer: " + computerScore);
+            result.textContent = " Score! " + humanChoice + " beats " + computerChoice + "!";
+            score.textContent = "You: " + humanScore + "\nComputer: " + computerScore;
+        }
+        
+        if (humanScore === 5) {
+            winner.textContent = "You Win!";
+            humanScore = 0;
+            computerScore = 0;
+        } else if (computerScore === 5) {
+            winner.textContent = "Computer Wins.";
+            humanScore = 0;
+            computerScore = 0;
+        } else {
+            winner.textContent = "";
         }
     }
-  
+    //line breaks not working, <br> doesn't work either
+
+    const result = document.querySelector('#result');
+    const score = document.querySelector('#score');
+    const winner = document.querySelector('#winner');
     const rock = document.querySelector('#rock');
     const paper = document.querySelector('#paper');
     const scissors = document.querySelector('#scissors');
@@ -39,12 +54,3 @@
     paper.addEventListener("click", () => playRound("Paper",computerChoice));
     scissors.addEventListener("click", () => playRound("Scissors",computerChoice));
     
-
-    if (humanScore === 5) {
-        console.log("You Win!")
-    } 
-    if (computerScore === 5) {
-        console.log("Computer Wins.")
-    }
-
-    // turn those into dom methods in the results div
